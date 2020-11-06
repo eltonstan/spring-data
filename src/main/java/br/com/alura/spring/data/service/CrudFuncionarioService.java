a7867c64a71b27453ff1f2b1937cde960bda61f6
Elton Standerski <eltonstan@yahoo.com.br>
2020-11-06 14:28:01 -0300

    Aula 04: paginacao repository

--- src/main/java/br/com/alura/spring/data/service/CrudFuncionarioService.java
+++ src/main/java/br/com/alura/spring/data/service/CrudFuncionarioService.java
@@ -8,6 +8,10 @@ import java.util.Optional;
 import java.util.Scanner;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import br.com.alura.spring.data.orm.Cargo;
@@ -51,7 +55,7 @@ public class CrudFuncionarioService {
 				atualizar(scanner);
 				break;
 			case 3:
-				visualizar();
+				visualizar(scanner);
 				break;
 			case 4:
 				deletar(scanner);
@@ -147,8 +151,17 @@ public class CrudFuncionarioService {
         System.out.println("Alterado");
 	}
 	
-	private void visualizar() {
-		Iterable<Funcionario> funcionarios = funcionarioRepository.findAll();
+	private void visualizar(Scanner scanner) {
+		System.out.println("Qual pagina você deseja visualizar?");
+		Integer pageInteger = scanner.nextInt();
+		
+		Pageable pageable = PageRequest.of(pageInteger, 2, Sort.by(Sort.Direction.ASC, "nome"));
+		Page<Funcionario> funcionarios = funcionarioRepository.findAll(pageable);
+		
+		System.out.println(funcionarios);
+		System.out.println("Pagina atual " + funcionarios.getNumber());
+		System.out.println("Total elemento " + funcionarios.getTotalElements());
+		System.out.println("Total Paginas " + funcionarios.getTotalPages());
 		funcionarios.forEach(funcionario -> System.out.println(funcionario));
 	}
 	
